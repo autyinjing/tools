543da0e5a27d3229c36a4be8c605998941724c22
aut <aut@localhost.localdomain>
2017-03-10 17:14:17 +0800

    add test

---
+++ cmake/step1/main.cc
@@ -0,0 +1,18 @@
+/* ***********************************************************************
+
+  > File Name: main.cc
+  > Author: Aut
+  > Mail: aut@taomee.com 
+  > Created Time: 2017年02月27日 星期一 18时26分53秒
+
+ ********************************************************************** */
+
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
+}
+
